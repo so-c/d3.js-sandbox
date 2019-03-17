529b8648856629093a7eb7034d160ac6d7356967
so-c <so_c321@hotmail.co.jp>
2019-03-17 10:30:18 +0900

    bokeh example.

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,5 +1,5 @@
 (function() {
-    var dataset = [
+    var data = [
         [220, 506],
         [950, 1804],
         [1200, 1116],
@@ -10,12 +10,37 @@
         [626, 821],
         [808, 402],
         [300, 940],
-    ],
-    width = 800,
-    height = 600,
-    margin = { "top": 30, "bottom": 60, "right": 30, "left": 60},
-    svg = d3.select("#svg");
+    ];
+    var svg = d3.select("#graph");
 
-    alert(svg);
+    svg.append("g").call(xAxis);
+    svg.append("g").call(yAxis);
 
+    margin = ({ top: 20, right: 30, bottom: 30, left: 40 })
+
+    x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.x)).nice()
+        .range([margin.left, width - margin.right]);
+
+    xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+            .attr("x", width - margin.right)
+            .attr("y", -4)
+            .attr("fill", "#000")
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "end")
+            .text(data.x))
+
+    yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.select(".tick:last-of-type text").clone()
+            .attr("x", 4)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(data.y))
 })();
